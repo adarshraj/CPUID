a7214742cdd68b0b2c59a810577a6c9e3d3ee2fd
Adarsh Raj <49880281+adarshraj@users.noreply.github.com>
2023-12-02 09:57:11 +0530

    Initial Commit

---
+++ Template.asm
@@ -0,0 +1,78 @@
+; ллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллл
+; General Asm Template by Lahar 
+; ллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллл
+
+.686					;Use 686 instuction set to have all inel commands
+.model flat, stdcall	;Use flat memory model since we are in 32bit 
+option casemap: none	;Variables and others are case sensitive
+.mmx
+
+
+
+include Template.inc	;Include our files containing libraries
+
+; ллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллл
+; Our initialised variables will go into in this .data section
+; ллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллл
+.data
+	szAppName	db	"Application Name",0
+	szFirstValue	dd	10.10
+	szSecondValue	dd	20.10
+	szThirdValue	dd	10.20
+	szFourthValue	dd	20.20
+
+; ллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллл
+; Our uninitialised variables will go into in this .data? section
+; ллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллл
+.data?
+	hInstance	HINSTANCE	?
+
+; ллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллл
+; Our constant values will go onto this section
+; ллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллл
+.const
+	IDD_DLGBOX	equ	1001
+	IDC_EXIT	equ	1002
+	APP_ICON	equ	2000
+
+; ллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллл
+; This is the section to write our main code
+; ллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллл
+.code
+
+start:	
+	invoke GetModuleHandle, NULL
+	mov hInstance, eax
+	invoke InitCommonControls
+	invoke DialogBoxParam, hInstance, IDD_DLGBOX, NULL, addr DlgProc, NULL
+	invoke ExitProcess, NULL
+
+DlgProc		proc	hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
+	.if uMsg == WM_INITDIALOG
+		invoke SetWindowText, hWnd, addr szAppName
+		invoke LoadIcon, hInstance, APP_ICON
+		invoke SendMessage, hWnd, WM_SETICON, 1, eax
+		cpuid
+		rdtsc
+		finit
+		fld szFirstValue
+		fmul szSecondValue
+		fld szThirdValue
+		fmul szFourthValue
+		FADD 
+	;	PADDB
+	.elseif uMsg == WM_COMMAND
+		mov eax, wParam
+		.if eax == IDC_EXIT
+			invoke SendMessage, hWnd, WM_CLOSE, 0, 0
+		.endif
+	.elseif uMsg == WM_CLOSE
+		invoke EndDialog, hWnd, NULL
+	.endif
+	
+	xor eax, eax				 
+	Ret
+DlgProc EndP
+
+end start	
+	 
